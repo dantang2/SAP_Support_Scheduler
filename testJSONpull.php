3a7feb6f2416cf8604d33831869b10ff7e1d8d09
dantang2 <dantang96@gmail.com>
2018-11-12 16:56:43 -0800

    php write to json complete

--- testJSONpull.php
+++ testJSONpull.php
@@ -3,8 +3,18 @@
   $data = json_decode($jsonString, true);
 
   print_r($data);
+  echo sizeof($data);
   echo "<br><br>";
-  print_r($data['Engineers'][0]['EngineerName']);
+  //print_r($data['Engineers'][0]['fname']);
+
+  $ken = Array('fname'=>'Kenneth','lname'=>'Tsang','team'=>'WEBI','email'=>'kenneth.tsang@sap.com');
+
+  array_push($data['Engineers'],$ken);
+
+  print_r($data);
+
+  $newJsonString = json_encode($data);
+  file_put_contents('./webapp/Engineers.json', $newJsonString);
 
 
 ?>
